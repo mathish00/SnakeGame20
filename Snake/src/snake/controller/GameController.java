1893cf31ffd9fb09510548ab04f15fcc867ac3e9
mhoff <mhoff@DESKTOP-3VK3FV6.fritz.box>
2020-06-14 13:51:19 +0200

    Refactor the gitTest() method

--- Snake/src/snake/controller/GameController.java
+++ Snake/src/snake/controller/GameController.java
@@ -8,10 +8,13 @@ public class GameController {
 	int height;
 	int width;
 	
+	String str = "this is another version of the project which is have just edited";
+	
 	Snake snake = new Snake(13, 15, Direction.RIGHT, this);
 
 	private void gitTest() {
 		System.out.println("Theres the push!");
+		System.out.println(str);
 
 	}
 }
